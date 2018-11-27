d87be9fc82fa4e814aac446cdd4c1c15fcc6483e
Katsu <true_love_everyone@yahoo.co.jp>
2018-11-28 01:25:41 +0900

    made new prototype

--- java/temp/Library.java
+++ java/temp/Library.java
@@ -19,6 +19,18 @@ public class Library {
 		// TODO Auto-generated method stub
 		Library library = new Library("SunMoonLibrary");
 		System.out.println("어서오세요.");
+		library.signUpBook();
+		library.signUpBorrower();
+		library.searchBook();
+//		Book book = borrower.search(library, librarian, bookName);
+//		if(book != null && borrower.checkLoanStatus(library, librarian, book)) {
+//			//Book클레스 말고 cNumber로?
+//			borrower.borrowBook(library, librarian, book);
+//			borrower.returnBook(library, librarian, book);
+//		}
+	}
+
+	public void signUpBook() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("登録する本のジャンルと名前、著者を入力してください。");
 		System.out.println("0.総記 1.哲学 2.歴史 3.社会科学 4.自然科学 5.技術 6.産業 7.芸術 8.言語 9.文学");
@@ -28,35 +40,16 @@ public class Library {
 		String bookName = input.nextLine();
 		System.out.println("책 저자 입력");
 		String author = input.nextLine();
-		cNumber = library.makeCNumber(bookName, author, cNumber);
+		cNumber = this.makeCNumber(bookName, author, cNumber);
 		System.out.println(cNumber);
 		Book book = new Book(bookName, author, cNumber);
-		library.signUpBook(book);
-		System.out.println("お客様のお名前を入力してください。");
-		String borrowerName = input.nextLine();
-		library.signUpBorrower(borrowerName);
-//		System.out.println("책 제목을 입력하세요.");
-		System.out.println("検索する本のジャンルと名前、著者を入力してください。");
-		System.out.println("0.総記 1.哲学 2.歴史 3.社会科学 4.自然科学 5.技術 6.産業 7.芸術 8.言語 9.文学");
-		System.out.println("종류 입력(수자)");
-		cNumber = input.nextLine();
-		System.out.println("책 제목 입력");
-		bookName = input.nextLine();
-		System.out.println("책 저자 입력");
-		author = input.nextLine();
-//		Book book = borrower.search(library, librarian, bookName);
-//		if(book != null && borrower.checkLoanStatus(library, librarian, book)) {
-//			//Book클레스 말고 cNumber로?
-//			borrower.borrowBook(library, librarian, book);
-//			borrower.returnBook(library, librarian, book);
-//		}
+		this.books.get(Integer.parseInt(book.getCNumber().substring(0, 1))).add(book);
 		input.close();
 	}
-
-	public void signUpBook(Book book) {
-		this.books.get(Integer.parseInt(book.getCNumber().substring(0, 1))).add(book);
-	}
-	public void signUpBorrower(String borrowerName) {
+	public void signUpBorrower() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("お客様のお名前を入力してください。");
+		String borrowerName = input.nextLine();
 		boolean check = false;
 		for(Borrower x: this.borrowers) {
 			if(x.getName().equals(borrowerName)) {
@@ -70,6 +63,7 @@ public class Library {
 			Borrower b = new Borrower(borrowerName);
 			this.borrowers.add(b);
 		}
+		input.close();
 	}
 	public String checkBooks(String bookName, String author, String cNumber) {
 		String No = "00";
@@ -97,5 +91,28 @@ public class Library {
 		}
 		cNumber += this.checkBooks(bookName, author, cNumber);
 		return cNumber;
+	}
+	public void searchBook() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("検索する本のジャンルと名前、著者を入力してください。");
+		System.out.println("0.総記 1.哲学 2.歴史 3.社会科学 4.自然科学 5.技術 6.産業 7.芸術 8.言語 9.文学");
+		System.out.println("종류 입력(수자)");
+		String cNumber = input.nextLine();
+		System.out.println("책 제목 입력");
+		String bookName = input.nextLine();
+		System.out.println("책 저자 입력");
+		String author = input.nextLine();
+		if(cNumber.equals("")) {
+			for(ArrayList<Book> x: this.books) {
+				for(Book y: x) {
+					
+				}
+			}
+		}else {
+			for(Book b: this.books.get(Integer.parseInt(cNumber))) {
+				
+			}
+		}
+		input.close();
 	}
 }
