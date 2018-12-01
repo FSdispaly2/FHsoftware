bb5501a6085b23710d8e8ed1c2e0bf9ad01517d1
Katsu <true_love_everyone@yahoo.co.jp>
2018-12-01 11:23:25 +0900

    error

--- java/temp/temp/Library.java
+++ java/temp/temp/Library.java
@@ -5,30 +5,30 @@ public class Library {
 
 	private String name;
 //	private ArrayList<Book> books = new ArrayList<Book>();
-	private HashSet<HashSet<Book>> books = new HashSet<HashSet<Book>>();
-	private HashSet<Borrower> borrowers = new HashSet<Borrower>();
-	private HashSet<String> librarians = new HashSet<String>();
-	private Iterator<Object> it;
+	private TreeSet<Book> books = new TreeSet<Book>();
+	private TreeSet borrowers = new TreeSet();
+	private TreeSet librarians = new TreeSet();
+	private Iterator iBook;
 	
 	public Library(String name) {
 		this.name = name;
-		for(int i = 0; i < 9; i++) {
-			this.books.add(new HashSet<Book>());
-		}
 	}
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		Library library = new Library("SunMoonLibrary");
-		library.login(input);
+//		library.login(input);
 		System.out.println("어서오세요 " + library.getName() + "입니다.");
 		library.signUpBook(input);
-		library.signUpBorrower(input);
+		library.signUpBook(input);
+		library.signUpBook(input);
+//		library.signUpBorrower(input);
+//		library.it = library.books.iterator().next().iterator();
 //		Book book = library.searchBook(input);
 //		library.it = library.books.iterator().next();
 //		Book book = library.it.;
-		library.borrowBook(input, book);
-		library.returnBook(book);
+//		library.borrowBook(input, book);
+//		library.returnBook(book);
 	}
 	
 	public String getName(){
@@ -47,31 +47,32 @@ public class Library {
 		cNumber = this.makeCNumber(bookName, author, cNumber);
 		System.out.println(cNumber);
 		Book book = new Book(bookName, author, cNumber);
-		this.books.get(Integer.parseInt(book.getCNumber().substring(0, 1))).add(book);
+		this.books.add(book);
 	}
-	public void signUpBorrower(Scanner input) {
-		System.out.println("대출자의 이름을 입력해주세요.");
-		String borrowerName = input.nextLine();
-		boolean check = false;
-		for(Borrower x: this.borrowers) {
-			if(x.getName().equals(borrowerName)) {
-				check = true;
-				break;
-			}
-		}
-		if(check) {
-			System.out.println("이미 등록된 이름입니다.");
-		} else {
-			Borrower b = new Borrower(borrowerName);
-			this.borrowers.add(b);
-		}
-	}
-	public String checkBooks(String bookName, String author, String cNumber) {
+//	public void signUpBorrower(Scanner input) {
+//		System.out.println("대출자의 이름을 입력해주세요.");
+//		String borrowerName = input.nextLine();
+//		boolean check = false;
+//		for(Borrower x: this.borrowers) {
+//			if(x.getName().equals(borrowerName)) {
+//				check = true;
+//				break;
+//			}
+//		}
+//		if(check) {
+//			System.out.println("이미 등록된 이름입니다.");
+//		} else {
+//			Borrower b = new Borrower(borrowerName);
+//			this.borrowers.add(b);
+//		}
+//	}
+	public String checkBooks(String cNumber) {
 		String No = "00";
-		ArrayList<Book> temp = this.books.get(Integer.parseInt(cNumber.substring(0, 1)));
+		this.iBook = this.books.iterator();
 		int count = 0;
-		for(Book x: temp) {
-			if(cNumber.equals(x.getCNumber().substring(0, cNumber.length()))) {
+		while(this.iBook.hasNext()) {
+			Book b = (Book)this.iBook.next();
+			if(cNumber.equals(b.getCNumber().substring(0, cNumber.length()))) {
 				count++;
 			}
 		}
@@ -90,7 +91,7 @@ public class Library {
 		for(char x: ctemp) {
 			cNumber += (int)(x);
 		}
-		cNumber += this.checkBooks(bookName, author, cNumber);
+		cNumber += this.checkBooks(cNumber);
 		return cNumber;
 	}
 //		Book book = null;
@@ -115,34 +116,34 @@ public class Library {
 //		}
 //		return book;
 //	}
-	public boolean login(Scanner input) {
-		System.out.println("ID를 입력해주세요.");
-		String ID = input.nextLine();
-		for(String x: this.librarians) {
-			if(x.equals(ID)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	public void borrowBook(Scanner input, Book book) {
-		System.out.println("대출자의 이름을  입력해주세요 히하");
-		String signIn = input.nextLine();
-		boolean check = true;
-		for(Borrower x: this.borrowers) {
-			if(signIn.equals(x.getName()) && book.loanStatus()) {
-				book.connect(x);
-				System.out.println("처리가 완료되었습니다.");
-				check = false;
-				break;
-			}
-		}
-		if(check) {
-			System.out.println("error");
-		}
-	}
-	public void returnBook(Book book) {
-		book.disconnect();
-		System.out.println("처리가 완룍되었습니다.");
-	}
+//	public boolean login(Scanner input) {
+//		System.out.println("ID를 입력해주세요.");
+//		String ID = input.nextLine();
+//		for(String x: this.librarians) {
+//			if(x.equals(ID)) {
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+//	public void borrowBook(Scanner input, Book book) {
+//		System.out.println("대출자의 이름을  입력해주세요 히하");
+//		String signIn = input.nextLine();
+//		boolean check = true;
+//		for(Borrower x: this.borrowers) {
+//			if(signIn.equals(x.getName()) && book.loanStatus()) {
+//				book.connect(x);
+//				System.out.println("처리가 완료되었습니다.");
+//				check = false;
+//				break;
+//			}
+//		}
+//		if(check) {
+//			System.out.println("error");
+//		}
+//	}
+//	public void returnBook(Book book) {
+//		book.disconnect();
+//		System.out.println("처리가 완룍되었습니다.");
+//	}
 }
